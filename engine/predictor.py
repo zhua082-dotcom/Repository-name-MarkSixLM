2a91c847998b32fc08fd559bf741a9bc4bfbb093
zhua082-dotcom <zhua082@icloud.com>
2026-08-07 07:24:53 +0800

    更新 predictor.py

--- engine/predictor.py
+++ engine/predictor.py
@@ -2,5 +2,22 @@
 Prediction Engine
 """
 
+from history_loader import HistoryLoader
+from feature import FeatureEngine
+
 class Predictor:
-    pass
+
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+
+    def run(self):
+
+        history = HistoryLoader(self.csv_path).load()
+
+        feature = FeatureEngine(history)
+
+        df = feature.build()
+
+        print(df.head())
+
+        return df
